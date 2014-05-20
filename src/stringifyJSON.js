86f5b3d30abc7dbfe0140bfe77bf0e57bd736078
Imti <imtiazmaj@gmail.com>
2014-05-19 20:37:25 -0400

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) return '' + obj + ''; // take care of pesky null special case
+  switch(typeof obj) {
+    case 'undefined':
+		case 'function':
+			return undefined;
+
+		case 'string':
+			return '"' + obj + '"';
+
+		case 'number':
+		case 'boolean':
+			return '' + obj + '';
+	}
+
+	if(obj instanceof Array) { // is it an array?
+		var output = [];
+		_.each(obj, function(elem) {
+			if(stringifyJSON(elem) === undefined)
+			  output.push('null');
+		  else
+        output.push(stringifyJSON(elem));
+		});
+    return "[" + output + "]";
+
+	} else { //it must be an object then
+
+    var output = "";
+    _.each(obj, function(elem, index) {
+      if(output !== "") 
+			  output += ",";
+		  
+		  if(stringifyJSON(elem) !== undefined)
+				output += stringifyJSON(index) + ":" + stringifyJSON(elem);
+		});
+    return "{" + output + "}";
+	}
 };
